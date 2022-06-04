2ecb6461c52754530e4386e116eb4c13ee2416d3
osmnc <osmn.cn87@hotmail.com>
2022-06-04 16:34:56 +0300

    CreateNewJob's Component is developing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import CreateNewJob from "./CreateNewJob";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <CreateNewJob />
+    </>
+  );
 }
 
 export default App;
